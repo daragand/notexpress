da15cc777b0b09934a78c923e6aa86eda5be5998
Demba Thiam <daragan.dooms@gmail.com>
2023-09-28 12:07:35 +0200

    ✅ ajout des fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\DataFixtures;
+use Faker\Factory;
+use App\Entity\Category;
+use App\Entity\Note;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create($fakerLocale = 'fr_FR');
+
+
+/**
+         * Les users
+         * Traitement pour l'ajout des utilisateurs
+         */
+
+         $objectUser = [];
+
+         for ($i = 0; $i < 100; $i++) {
+            $user = new User();
+            $user->setUsername($faker->userName())
+                ->setPassword($faker->password())
+              ;
+            // On ajout l'objet utilisateur dans le tableau
+            array_push($objectUser, $user);
+            // On persiste l'objet utilisteurs
+            $manager->persist($user);
+        }
+
+
+
+/**
+         * Les users
+         * Traitement pour l'ajout des utilisateurs
+         */
+
+
+
+        $categories = [
+            'Travail',
+            'Logement',
+            'Bricolage',
+            'Administratif',
+            'Formation',
+        ];
+
+        $objectCategory = [];
+
+        for ($i = 0; $i < count($categories); $i++) {
+            $category = new Category();
+            $category->setName($categories[$i])
+                ->setUser($objectUser[rand(0, 99)]);
+            // On ajoute l'objet catégorie dans le tableau
+            array_push($objectCategory, $category);
+            // On persiste l'objet catégorie
+            $manager->persist($category);
+        }
+
+        
+        /**
+         * Les notes
+         * Traitement pour l'ajout des notes
+         */
+
+
+
+
+         $objectNotes = [];
+
+         for ($i = 0; $i < 100; $i++) {
+            $note = new Note();
+            $note->setTitle($faker->userName())
+                ->setContent($faker->sentence(10))
+                ->setCategory($objectCategory[rand(0, 4)])
+                ->setUser($objectUser[rand(0, 99)])
+              ;
+            // On ajout la note  dans le tableau
+            array_push($objectNotes, $note);
+            // On persiste l'objet note
+            $manager->persist($note);
+        }
+
+
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
